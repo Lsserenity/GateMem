6e06b56e702773db27f646804a9fb8fb98c17629
BZPnx <zxt991111@gmail.com>
2025-12-29 20:57:01 +0800

    刚刚少交了一点东西，现在补上来，已经完全符合README中结构

---
+++ memory_network.py
@@ -0,0 +1,88 @@
+import torch
+from torch import nn, optim
+from torch.nn.functional import normalize
+from torch.func import functional_call
+
+# 定义类似titans的NeuralMemory模块
+class NeuralMemory(nn.Module):
+
+    def __init__(self, input_dim = 16, layers = 2, hidden_dim = 32, alpha = 0.999, eta = 0.60, theta = 0.05):
+        super().__init__()
+        self.layers = None
+        # 定义MLP层结构
+        if layers == 1:
+            self.layers = nn.ModuleList([nn.Linear(input_dim, input_dim)])
+        else:
+            self.layers = nn.ModuleList([])
+            for i in range (layers - 1):
+                self.layers.append(nn.Sequential(
+                    nn.Linear(input_dim, hidden_dim),
+                    nn.SiLU()
+                ))
+            self.layers.append(nn.Sequential(
+                nn.Linear(hidden_dim, input_dim),
+                nn.SiLU()
+            ))
+
+        # 定义key和value的参数层
+        self.K = nn.Linear(input_dim, input_dim, bias = False)
+        self.V = nn.Linear(input_dim, input_dim, bias = False)
+        self.Q = nn.Linear(input_dim, input_dim, bias = False)
+
+        # 初始化key和value层的权重
+        torch.nn.init.xavier_uniform_(self.K.weight)
+        torch.nn.init.xavier_uniform_(self.V.weight)
+        torch.nn.init.xavier_uniform_(self.Q.weight)
+
+        # 定义参数更新相关的超参数
+        self.alpha = alpha
+        self.eta = eta
+        self.theta = theta
+
+        # 存储惊讶度
+        self.surprise = {}
+        self.silu = nn.SiLU()
+    
+    # 定义前向传播函数
+    def forward(self, X):
+        for layer in self.layers:
+            X = layer(X)
+        return X
+
+    # 定义记忆查询函数
+    def retrieve(self, X, new_params = None):
+        if new_params is None:
+            params = dict(self.named_parameters())
+        else:
+            params = new_params
+        return functional_call(self, params, self.silu(normalize(self.Q(X))))
+    
+    # 定义记忆更新函数
+    def update(self, X):
+        x = X.detach()
+
+        # 计算k和v
+        k = normalize(self.silu(self.K(x)))
+        v = self.silu(self.V(x))
+
+        for layer in self.layers:
+            x = layer(x)
+        
+        # 计算loss和梯度
+        loss = ((k - v)**2).mean(axis=0).sum()
+        grads = torch.autograd.grad(loss, self.parameters(), create_graph=True)
+
+        # 更新mlp的参数
+        update_params = {}
+        for (name, param), grad in zip(self.named_parameters(), grads):
+            if name not in self.surprise:
+                self.surprise[name] = torch.zeros_like(grad)
+            self.surprise[name] = self.eta * self.surprise[name] - self.theta * grad
+            # 只更新mlp的参数
+            if name[0] not in ['K', 'V', 'Q']:
+                updated_param = self.alpha * param.data + self.surprise[name]
+                update_params[name] = updated_param
+            else:
+                update_params[name] = param.data
+        
+        return loss.item(), update_params
